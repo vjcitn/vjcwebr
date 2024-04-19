bc6e5a5bf37b1bcf5b80bb88206cc9ebb93721bb
vjcitn <stvjc@channing.harvard.edu>
2024-04-19 06:31:52 -0400

    problem with devtools::document, try other machine

---
+++ R/data.R
@@ -0,0 +1,62 @@
+#' geographic information from statcrunch.com on metropolitan statistical areas
+#' @docType data
+#' @format data.frame
+"statcrunch_msa"
+
+
+#' cancer rate data by metropolitan statistical area, 1999-2018, from CDC WONDER system
+#' @docType data
+#' @format data.frame
+#' @note Created by joining statcrunch location data to result of WONDER request.
+#' See woncan_meta for details.
+"woncan"
+
+#' metadata on cancer rate data by metropolitan statistical area, 1999-2018, from CDC WONDER system
+#' @docType data
+#' @format character
+"woncan_meta"
+
+#' a table of data of Vince's contacts who developed cancer
+#' @docType data
+#' @format data.frame
+"vjc_cancer_net"
+
+#' a table of cancer rate data by country, retrieved from https://gco.iarc.fr/today/online-analysis-map
+#' @docType data
+#' @format data.frame
+"world_cancer_2020_all"
+
+
+#' a data.frame with clinical features of a breast cancer study (METABRIC) derived from cBioPortal
+#' @docType data
+#' @format data.frame
+"metabric"
+
+#' a data.frame with clinical features of a metastatic prostate adenocarcinoma study
+#' @docType data
+#' @format data.frame
+"prostate_met_adeno"
+
+#' a data.frame with (rounded) crude rates of cancer as presented at https://www.statcan.gc.ca/en/dai/btd/asr 
+#' @docType data
+#' @format data.frame
+"canada_crude"
+
+#' a data.frame with SEER estimates of cancer incidence for 2018
+#' @docType data
+#' @format data.frame
+#' @note Collected at \url{https://seer.cancer.gov/statistics/preliminary-estimates/data-downloads/Feb-2019-Preliminary-Estimates.xlsx} on May 27 2022.  Notes include: "The estimates in this file are preliminary estimates for the November 2019 SEER submission based on the February 2019 SEER submission. These rates are subject to change prior to the official SEER release of estimates in April 2020.
+#' Source: SEER 18 areas (San Francisco, Connecticut, Detroit, Hawaii, Iowa, New Mexico, Seattle, Utah, Atlanta, San Jose-Monterey, Los Angeles, Alaska Native Registry, Rural Georgia, California excluding SF/SJM/LA, Kentucky, Louisiana, New Jersey and Georgia excluding ATL/RG).
+#' Rates are per 100,000 and are age-adjusted to the 2000 US Std Population (19 age groups - Census P25-1130).
+"SEER_2018"
+
+#' a SummarizedExperiment collecting BRCA mutations and colData from curatedTCGAData
+#' @docType data
+#' @format SummarizedExperiment
+"brcamutSE"
+
+#-rw-r--r--  1 vincentcarey  staff   31905 Mar 31 07:34 prostate_met_adeno.rda
+#-rw-r--r--  1 vincentcarey  staff   79469 Mar 31 06:24 metabric.rda
+#-rw-r--r--  1 vincentcarey  staff     444 Mar 20 14:14 vjc_cancer_net.rda
+#-rw-r--r--  1 vincentcarey  staff  814098 Mar 20 08:56 world_geo_sf.rda
+#-rw-r--r--  1 vincentcarey  staff    2787 Mar 20 08:52 world_cancer_2020_all.rda
---
+++ R/tabs.R
@@ -0,0 +1,41 @@
+#' simple tables of confidence intervals for cancer incidence
+#' @return a CSV file with columns as provided via \url{https://gis.cdc.gov/CANCER/USCS/#/StateCounty/}
+#' @param site character(1) name of anatomic site, either "breast" or "prostate"
+#' @param simple logical(1) defaults to TRUE, omits state and sex for demonstrative tables
+#' @examples
+#' btab = MA_cancer_rate_table("breast")
+#' dim(btab)
+#' names(btab)
+#' @export
+MA_cancer_rate_table = function(site="breast", simple=TRUE) {
+ stopifnot(site %in% c("breast", "prostate"))
+ if (site == "prostate") ans = read.csv(system.file("csv/MASSProstateWCI.csv", package="YESCDS"))
+ else ans = read.csv(system.file("csv/MABreastWCI.csv", package="YESCDS"))
+ if (simple) ans = ans[, -c(1,5)]
+ ans
+}
+
+#' generate vector of cancer sites in CDC Wonder table
+#' @export
+woncan_types = function() {
+  data("woncan", package="YESCDS")
+  sort(unique( woncan$`Cancer Sites`))
+}
+
+#' generate site-specific rate table
+#' @importFrom dplyr filter mutate
+#' @param site character(1) name of anatomic site, must reside in value of `woncan_types()`
+#' @export
+filter_woncan = function(site) {
+  stopifnot(site %in% woncan_types())
+  woncan |> dplyr::filter(`Cancer Sites` == site) |> dplyr::mutate(aarate=Age.Adjusted.Rate)
+}
+
+
+#' simplify table view of WONDER CDC statistsics
+#' @param site character(1) name of anatomic site, must reside in value of `woncan_types()`
+#' @export
+table_woncan = function(site) {
+  data("woncan", package="YESCDS")
+  DT::datatable(filter_woncan(site))
+}
